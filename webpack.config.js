03df170ae52f9b67b7e94ec00106020ac7ffcd94
Sergei Galkovskii <sergei.galkovskii@gmail.com>
2017-03-20 19:00:13 +0200

    Added typescript into project

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig =
+const webpack = require("webpack");
 
+module.exports = {
+    entry: './index.tsx',
+    devtool: "source-map",
+    output: {
+        path: path.resolve('dist'),
+        publicPath: "/",
+        filename: 'index.[hash].js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            {
+                test: /\.tsx?$/,
+                enforce: "pre",
+                loader: "tslint-loader"
 
-    module.exports = {
-        entry: './index.jsx',
-        output: {
-            path: path.resolve('dist'),
-            filename: 'index.[hash].js'
-        },
-        module: {
-            loaders: [
-                { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-                { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-            ]
-        },
-        resolve: {
-            extensions: [".js", ".jsx"]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './index.html',
-                filename: 'index.html',
-                inject: 'body'
-            })
+            },
+            {
+                test: /\.tsx?$/,
+                use: ["ts-loader"]
+            },
         ]
-    }
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", "js"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+}
